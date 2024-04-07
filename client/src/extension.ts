c7195abb5a1077a01fafbbd07b735a302f61d2e4
Electro_593 <electro5939@gmail.com>
2024-04-07 17:21:43 -0400

    JSON, LSP, RPC

---
+++ client/src/extension.ts
@@ -0,0 +1,53 @@
+import * as path from 'path';
+import {workspace, ExtensionContext} from 'vscode';
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+  const serverProcess = context.asAbsolutePath(
+    path.join('server', 'out', 'server.exe')
+  );
+  
+  let serverOptions: ServerOptions = {
+    run: {
+      command: serverProcess,
+      transport: TransportKind.stdio
+    },
+    debug: {
+      command: serverProcess,
+      transport: TransportKind.stdio
+    }
+  };
+  
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: [
+      {
+        scheme: 'file',
+        language: 'jai'
+      }
+    ],
+    synchronize: {
+      fileEvents: workspace.createFileSystemWatcher('**/*.jai')
+    }
+  };
+  
+  client = new LanguageClient(
+    'jaiLanguageServer',
+    'Jai Language Server',
+    serverOptions,
+    clientOptions
+  );
+  
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  return client?.stop();
+}
